bc9f4fe135c5560eebb55e157bc850f4e62de8cb
Togrul85 <toghrulkhs@code.edu.az>
2023-03-16 17:53:07 +0400

    SignalRRecord1

--- JuanProject/Program.cs
+++ JuanProject/Program.cs
@@ -1,4 +1,5 @@
 using JuanProject.DAL;
+using JuanProject.Hubs;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,14 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+
+
+
+
+
+
+builder.Services.AddSignalR();
 
 
 var app = builder.Build();
@@ -34,6 +43,9 @@ app.MapControllerRoute(
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/chat");
+
 
 
 
